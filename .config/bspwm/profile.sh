b7313d1d58282794164136c25b1794b953b001ea
remfly <remfly@protonmail.com>
2022-08-04 09:45:09 -0300

    Update for laptop settings

--- .config/bspwm/profile.sh
+++ .config/bspwm/profile.sh
@@ -20,16 +20,16 @@ external_monitor="$(xrandr --query | grep -v 'disconnected' | grep 'HDMI-2')"
 
 if [[ "${external_monitor}" = *connected* ]]; then
     xrandr --output eDP-1 --off --output HDMI-2 --primary --rate 120 --mode 1920x1080 --pos 0x0 --rotate normal &
-    bspc monitor HDMI-2 -d "" "" "" "ﱘ" "" "" "﬐" "" "" &
+    bspc monitor HDMI-2 -d "" "" "" "ﱘ" "" "" "﬐" "" "" &
     /home/remfly/.config/polybar/monitor.sh &
     /usr/bin/dunst -config /home/remfly/.config/dunst/monitor_dunstrc &
     xgamma -rgamma 1,1 -ggamma 1,1 -bgamma 1,1 &
     exec setxkbmap -layout us -variant altgr-intl &
 else
-    xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-2 --off &
-    bspc monitor eDP-1 -d "" "" "" "ﱘ" "" "" "﬐" "" "" &
+    xrandr --output eDP-1 --primary --mode 1920x1080 --rate 60 --pos 0x0 --rotate normal --output HDMI-2 --off &
+    bspc monitor eDP-1 -d "" "" "" "ﱘ" "" "" "﬐" "" "" &
     /home/remfly/.config/polybar/laptop.sh &
     /usr/bin/dunst -config /home/remfly/.config/dunst/laptop_dunstrc &
-    xgamma -rgamma 0.8,0.8 -ggamma 0.8,0.8 -bgamma 0.75,0.75 &
+    xgamma -rgamma 0.75,0.75 -ggamma 0.75,0.75 -bgamma 0.72,0.72 &
     exec setxkbmap -layout br &
 fi
