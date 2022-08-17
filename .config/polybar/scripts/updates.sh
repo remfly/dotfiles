359de693b1074afa23ece5b305a4de982e7a76a5
remfly <remfly@protonmail.com>
2022-08-17 11:03:22 -0300

    Fix the polybar update module

--- .config/polybar/scripts/updates.sh
+++ .config/polybar/scripts/updates.sh
@@ -19,7 +19,7 @@ if ! updates=$(checkupdates 2> /dev/null | wc -l ); then
     updates=0
 fi
 
-if [ "$updates" -gt 0 ]; then
+if [ "$updates" -ge 0 ]; then
     echo "UPDATES: $updates"
 else
     echo "UPDATES: N/A"
